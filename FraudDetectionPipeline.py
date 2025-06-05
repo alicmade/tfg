f33e58bf10b48594ce305cd2d4ee5b65b6a59112
alicmade <alicmade@myuax.com>
2025-06-05 11:09:00 +0200

    traigo el metodo para subir a google

--- FraudDetectionPipeline.py
+++ FraudDetectionPipeline.py
@@ -107,5 +107,45 @@ class FraudDetectionPipeline:
         print(f"Metrics saved to {os.path.abspath(file_path)}")
 
 
+    def upload_csv_to_google_sheets(self, csv_path, sheet_url, creds_path="credenciales.json"):
+
+
+                print("Iniciando carga de datos en Google Sheets...")
+                try:
+                    import csv, gspread
+                    from oauth2client.service_account import ServiceAccountCredentials
+                    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+                    creds = ServiceAccountCredentials.from_json_keyfile_name(creds_path, scope)
+                    client = gspread.authorize(creds)
+                    print("Autenticación completada")
+                except Exception as e:
+                    print(f"Error en autenticación: {e}")
+                    return
+
+                try:
+                    spreadsheet = client.open_by_url(sheet_url)
+                    worksheet = spreadsheet.get_worksheet(0)
+                    print("Hoja abierta correctamente")
+                except Exception as e:
+                    print(f"Error al abrir la hoja: {e}")
+                    return
+
+                try:
+                    with open(csv_path, 'r') as f:
+                        reader = csv.reader(f)
+                        data = list(reader)
+
+                    worksheet.update("A1", data)
+                    print("Datos subidos correctamente a Google Sheets")
+                except Exception as e:
+                    print(f"Error al subir los datos: {e}")
+
+    def export_results(self, csv_path="resultados_metricas.csv", sheet_url=None, creds_path="./credenciales.json"):
+
+                self.save_metrics_to_csv(sheet_url, csv_path)
+                if sheet_url:
+                    print("Subiendo resultados a Google Sheets..")
+                    self.upload_csv_to_google_sheets(csv_path, sheet_url, creds_path)
+
 
 
